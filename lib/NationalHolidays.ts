91fe88b2b322afb552e11ed64cbf8a8acd29479e
takasqr <95556025+takasqr@users.noreply.github.com>
2024-01-22 20:36:36 +0900

    Update NationalHolidays.ts

--- lib/NationalHolidays.ts
+++ lib/NationalHolidays.ts
@@ -1,9 +1,21 @@
 import { NationalHolidays } from './types';
 
 const nationalHolidays: NationalHolidays = [
-  { name: "建国記念日", date: '' },
-  { name: "海の日", date: '' },
-  // 他の都道府県データ...
+  { name: "元日", date: new Date(2024, 0, 1) }, // 1月1日
+  { name: "成人の日", date: new Date(2024, 0, 8) }, // 1月の第2月曜日
+  { name: "建国記念の日", date: new Date(2024, 1, 11) }, // 2月11日
+  { name: "春分の日", date: new Date(2024, 2, 20) }, // 春分日
+  { name: "昭和の日", date: new Date(2024, 3, 29) }, // 4月29日
+  { name: "憲法記念日", date: new Date(2024, 4, 3) }, // 5月3日
+  { name: "みどりの日", date: new Date(2024, 4, 4) }, // 5月4日
+  { name: "こどもの日", date: new Date(2024, 4, 5) }, // 5月5日
+  { name: "海の日", date: new Date(2024, 6, 15) }, // 7月の第3月曜日
+  { name: "山の日", date: new Date(2024, 7, 11) }, // 8月11日
+  { name: "敬老の日", date: new Date(2024, 8, 16) }, // 9月の第3月曜日
+  { name: "秋分の日", date: new Date(2024, 8, 22) }, // 秋分日
+  { name: "体育の日", date: new Date(2024, 9, 14) }, // 10月の第2月曜日
+  { name: "文化の日", date: new Date(2024, 10, 3) }, // 11月3日
+  { name: "勤労感謝の日", date: new Date(2024, 10, 23) } // 11月23日
 ];
 
 export default nationalHolidays
